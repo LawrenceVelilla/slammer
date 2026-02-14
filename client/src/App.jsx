27ad6e164324e3bec1b55becc898c6dd71cb4a05
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2026-02-14 10:55:58 -0700

    Add upload page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import SlammerLanding from "./components/SlammerLanding";
 import MissionPage from "./components/MissionPage";
+import UploadPage from "./components/UploadPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SlammerLanding />} />
         <Route path="/mission" element={<MissionPage />} />
+        <Route path="/upload" element={<UploadPage />} />
       </Routes>
     </HashRouter>
   );
